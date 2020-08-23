6286b748fae3e0d4859c09b73087e01a49fb0007
Christopher Wu <wu.christophert@gmail.com>
2020-08-23 00:41:51 -0700

    hide password fields in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
 
   validates_presence_of :name, :email, :dob, :password_digest
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  HIDDEN_FIELDS = [
+    'password_digest',
+    'encrypted_password',
+    'reset_password_token',
+    'reset_password_sent_at',
+    'remember_created_at'
+  ]
 
   self.implicit_order_column = 'created_at'
+
+  default_scope { select(User.column_names - HIDDEN_FIELDS) }
+
 end
